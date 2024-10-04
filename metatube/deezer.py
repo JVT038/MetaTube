4442569a26cf8968eb93cffc605e5013266ea39a
JVT038 <47184046+JVT038@users.noreply.github.com>
2024-10-04 22:07:21 +0200

    Update dependencies

--- metatube/deezer.py
+++ metatube/deezer.py
@@ -3,19 +3,19 @@ from metatube import sockets
 class Deezer():
        
     def socketsearch(data):
-        client = deezer.Client()
-        searchresults = client.search(data["title"], artist=data["artist"])
-        list = []
-        for result in searchresults:
-            list.append(result.as_dict())
-        maxlist = list[0:int(data["max"])]
-        maxlist.append(data["title"])
-        sockets.deezersearch(maxlist)
+        with deezer.Client() as client:
+            searchresults = client.search(data["title"], artist=data["artist"])
+            list = []
+            for result in searchresults:
+                list.append(result.as_dict())
+            maxlist = list[0:int(data["max"])]
+            maxlist.append(data["title"])
+            sockets.deezersearch(maxlist)
     
     def searchid(id):
-        client = deezer.Client()
-        return client.get_track(id).as_dict()
+        with deezer.Client() as client:
+            return client.get_track(id).as_dict()
     
     def sockets_track(id):
-        client = deezer.Client()
-        sockets.deezertrack(client.get_track(id).as_dict())
+        with deezer.Client() as client:
+            sockets.deezertrack(client.get_track(id).as_dict())
